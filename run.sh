1475f3e6fd9ca8d21c093afe3805dd56db834d10
Nicolai Abruzzese <nicoliniyo@gmail.com>
2024-06-06 12:08:57 -0400

    chore(development): add branding and i18n texts on index

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 source venv/bin/activate
+python manage.py collectstatic
+
+# Copiamos el directorio para i18n
+cp -r locale staticfiles/
+
 python manage.py runserver
 # python -m gunicorn adminapp.asgi:application -k uvicorn.workers.UvicornWorker
